1c488a4be903f57d17f825ab391cc01f9bed8deb
whateverhappenshappens <46918800+whateverhappenshappens@users.noreply.github.com>
2020-09-13 16:07:35 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="utkarsh is nice boy" msg2="ankit is gandu"/>
+  <hello/>
+  <HelloWorld msg="utkarsh is nice boy" msg2="ankit is gandu"/>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import hello from './components/hello.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/hello.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class=class1>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.class1{
+    height:500px;
+    background-color: aqua;
+    
+}
+</style>
